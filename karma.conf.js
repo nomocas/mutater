a65e457dc5a0a8bfaa4f4e9ef3ccf869f11b64d4
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-09 17:13:54 +0200

    lint karma.conf and rollup.config.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,81 +1,81 @@
 // Karma configuration
 // Generated on Tue Feb 07 2017 16:25:10 GMT+0100 (CET)
-var babel  = require('rollup-plugin-babel'),
-    babelrc = require('babelrc-rollup').default;
+const babel  = require('rollup-plugin-babel'),
+	babelrc = require('babelrc-rollup').default;
 
 module.exports = function(config) {
-    config.set({
+	config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+		basePath: '',
 
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['mocha', 'chai'],
+		frameworks: ['mocha', 'chai'],
 
 
         // list of files / patterns to load in the browser
-        files: [
+		files: [
             { pattern: 'src/**/*.js', included: false }, // watch them but do not include them
-            'test/**/*.js'
-        ],
+			'test/**/*.js'
+		],
 
 
         // list of files to exclude
-        exclude: [],
+		exclude: [],
 
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-            'src/**/*.js': ['rollup'],
-            'test/**/*.js': ['rollup']
-        },
-        rollupPreprocessor: {
-            plugins: [
-                babel(babelrc())
-            ],
-            moduleName:'mutater',
-            format: 'iife', // helps prevent naming collisions
-            sourceMap: 'inline', // sensible for testing
-        },
+		preprocessors: {
+			'src/**/*.js': ['rollup'],
+			'test/**/*.js': ['rollup']
+		},
+		rollupPreprocessor: {
+			plugins: [
+				babel(babelrc())
+			],
+			moduleName:'mutater',
+			format: 'iife', // helps prevent naming collisions
+			sourceMap: 'inline', // sensible for testing
+		},
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+		reporters: ['progress'],
 
 
         // web server port
-        port: 9876,
+		port: 9876,
 
 
         // enable / disable colors in the output (reporters and logs)
-        colors: true,
+		colors: true,
 
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+		logLevel: config.LOG_INFO,
 
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+		autoWatch: true,
 
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+		browsers: ['Chrome'],
 
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+		singleRun: false,
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
-    })
-}
+		concurrency: Infinity
+	});
+};
 
